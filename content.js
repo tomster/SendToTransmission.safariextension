9ac47cc0fb5fb249324fffc7bde6963a24bd439b
Tom Lazar <tom@tomster.org>
2011-01-01 21:19:27 +0100

    Make the finding of the clicked anchor more robust by traversing upwards in the DOM (to find anchors that wrap images or font tags etc.)

--- content.js
+++ content.js
@@ -6,11 +6,26 @@ var lastContextMenuEventTime;
 // Register for the contextmenu event.
 document.addEventListener("contextmenu", handleContextMenu, false);
 
+
+function findParentNode(parentTagName, childObj) {
+    var testObj = childObj;
+    while(testObj.tagName != parentTagName) {
+        testObj = testObj.parentNode;
+        if (testObj.tagName == 'HTML') {
+            return null;
+        };
+    }
+    return testObj;
+}
+
 function handleContextMenu(event)
 {
     lastRightClickedElement = event.target;
     lastContextMenuEventTime = new Date().getTime();
-    // Pass the right-clicked element's tag name, target url and the timestamp up to the global page.
-    safari.self.tab.setContextMenuEventUserInfo(event, { "tagName": event.target.tagName, 
-        "timestamp": lastContextMenuEventTime, "target_url": event.target.href });
+    target = findParentNode('A', event.target)
+    if (target != null) {
+        // Pass the right-clicked element's target url and the timestamp up to the global page.
+        safari.self.tab.setContextMenuEventUserInfo(event, {
+            "timestamp": lastContextMenuEventTime, "target_url": target.href });
+    }
 }
